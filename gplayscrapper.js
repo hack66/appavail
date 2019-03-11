cb246808247b1832df80831cd030068fecf0c243
Marios Isaakidis <prometheas@autistici.org>
2019-03-11 11:32:06 +0000

    Add ISO3166CC codes; Fix gplayscrapper arguments parsing

--- gplayscrapper.js
+++ gplayscrapper.js
@@ -1,9 +1,9 @@
 var gplay = require('google-play-scraper');
 
-var args = process.argv.slice(2);
+var args = process.argv;
 
 gplay.search({
-        term: args[1],
-        country: args[2],
-	lang: args[3] || 'en',
+        term: args[2],
+        country: args[3],
+	lang: args[4] || 'en',
       }).then(console.log, console.log);
